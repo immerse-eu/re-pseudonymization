c3ee59ab65d2d1e1f67c1247825a1378c676397f
krebswg <wolfgang.krebs@uk-erlangen.de>
2024-03-01 12:35:53 +0100

    Initial Commit

---
+++ main.py
@@ -0,0 +1,76 @@
+import pandas as pd
+import os
+import openpyxl
+import random
+import string
+
+
+# ToDo: Delete this method and remove comment to read .csv file in with re-pseudonymization from Threuhandstelle at UKER
+def create_new_pseudonyms(filename):
+    # read id data in
+    id_data = pd.read_excel("./input/" + filename)
+
+    # create new pseudonyms for original participant_ids
+    for index, row in id_data.iterrows():
+        id_data.loc[index, 1] = project_re_pseudo_id + ''.join(
+            random.choices(string.ascii_letters + string.digits, k=16))
+
+    # write id file with
+    id_data.to_excel("./output/All_IDs_for_repseudo.xlsx", index=False)
+
+    return id_data
+
+
+if __name__ == '__main__':
+    # specify filename for id file
+    id_filename = "Immerse-IDs_for_repseudo.csv"
+    # sensing data output subfolder name
+    sensing_data_output = "./output/sensing_data"
+    # specify pre-string for new pseudonyms
+    project_re_pseudo_id = "01_03_2024-Koppe-"
+    # specify all sensing filenames
+    sensing_file_names = ["Master_Activity.xlsx", "Master_AppUsage.xlsx", "Master_DeviceRunning.xlsx", "Master_DisplayOn.xlsx", "Master_Location.xlsx", "Master_NotificationLog.xlsx", "Master_Steps.xlsx"]
+    # specify all TherapyDesigner filenames
+    therapy_designer_file_names = ["Bierbeek_final.xlsx", "Bratislava_final.xlsx", "Kosice_final.xlsx", "Leuven_final.xlsx", "Lothian_CAMSH_final.xlsx", "Lothian_final.xlsx", "Mannheim_final.xlsx"]
+
+    # create not existing output subfolder for sensing data
+
+    if not os.path.exists(sensing_data_output):
+        os.makedirs(sensing_data_output)
+    # create not existing output subfolder for TherapyDesigner data
+    therapy_desinger_data_output = "./output/TherapyDesigner_data"
+    if not os.path.exists(therapy_desinger_data_output):
+        os.makedirs(therapy_desinger_data_output)
+
+    id_data = create_new_pseudonyms("Immerse-IDs_for_repseudo.xlsx")
+    # read in id data
+    #id_data = pd.read_csv(id_filename)
+
+    # iterate through subfolders and included files
+    for subdir, dirs, files in os.walk("./input"):
+        for file in files:
+            # do not use the id files but only the data files
+            if not (file == "Immerse-IDs_for_repseudo.csv" or file == "Immerse-IDs_for_repseudo.xlsx"):
+                print(file + " started.")
+                # read data file in
+                file_data = pd.read_excel(os.path.join(subdir, file))
+                # iterate through all rows of the id file and replace corresponding ids with new pseudonyms
+                for index, row in id_data.iterrows():
+                    file_data.replace(row.iloc[0], row.iloc[1], inplace=True)
+
+                    # print every 50 rows a message
+                    if index % 50 == 0:
+                        print(str(index+1) + " / " + str(len(id_data)) + " IDs replaced.")
+                # drop columns with identification data in sensing data
+                if file in sensing_file_names:
+                    file_data = file_data.drop(["StudyName", "probandID(moviID)"], axis=1)
+                    subfolder_for_export = "sensing_data/"
+
+                # drop columns with identification data in TheradyDesigner data
+                else:
+                    file_data = file_data.drop("study_name", axis=1)
+                    subfolder_for_export = "TherapyDesigner_data/"
+
+                # save file to output folder
+                file_data.to_excel("./output/" + subfolder_for_export + file, index=False)
+                print(file + " finished.")
